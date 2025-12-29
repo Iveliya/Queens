563aaff73659d6a99617df858f8ed490c3c53eec
Iveliya <iii_koleva@abv.bg>
2025-12-29 13:28:57 +0200

    Add board creation and memory management

--- QueensGame/QueensGame/QueensGame.cpp
+++ QueensGame/QueensGame/QueensGame.cpp
@@ -3,18 +3,56 @@
 
 #include <iostream>
 
-int main()
+const char EMPTY_CELL = '.';
+
+char** createBoard(int rows, int cols)
 {
-    std::cout << "Hello World!\n";
+    char** board = new char* [rows];
+
+    for (int r = 0; r < rows; r++)
+    {
+        board[r] = new char[cols];
+
+        for (int c = 0; c < cols; c++)
+        {
+            board[r][c] = EMPTY_CELL;
+        }
+    }
+
+    return board;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void destroyBoard(char** board, int rows)
+{
+    if (board == NULL)
+    {
+        return;
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (int r = 0; r < rows; r++)
+    {
+        delete[] board[r];
+    }
+
+    delete[] board;
+}
+
+int main()
+{
+    int rows = 5;
+    int cols = 6;
+
+    char** board = createBoard(rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            std::cout << board[i][j] << " ";
+        }std::cout << std::endl;
+    }
+
+    destroyBoard(board, rows);   
+    return 0;
+}
+
+
